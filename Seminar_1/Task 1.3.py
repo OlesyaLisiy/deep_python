272bd0475716889a50cdfd4c4f96c3959b4a6756
OlesyaLisiy <109387218+OlesyaLisiy@users.noreply.github.com>
2023-08-19 19:45:45 +0300

    Update Task 1.3.py

--- Seminar_1/Task 1.3.py
+++ Seminar_1/Task 1.3.py
@@ -7,19 +7,24 @@
 # num = randint(LOWER_LIMIT, UPPER_LIMIT)
 
 from random import randint
-random_value = randint(0, 1000)
-attempt = 0
-print("Загадано число от 0 до 1000. Попробуйте отгадать за 10 попыток")
-for i in range(1,11):
-    choice = int(input("Введите число: "))
-    if choice > random_value:
-        print("Это число больше задуманного")
-    elif choice < random_value:
-        print("Это число меньше задуманного")
-    else:
-        print(f"Верно! Количество попыток - {i}")
-        break
-    attempt += 1
-    print(f"Осталось попыток {10-attempt}")
-if attempt >= 10:
-    print(f"Попытки закончились! Было загадано {random_value}")
+
+def random_value_game():
+    random_value = randint(0, 1000)
+    attempt = 0
+    print("Загадано число от 0 до 1000. Попробуйте отгадать за 10 попыток")
+    for i in range(1,11):
+        choice = int(input("Введите число: "))
+        if choice > random_value:
+            print("Это число больше задуманного")
+        elif choice < random_value:
+            print("Это число меньше задуманного")
+        else:
+            print(f"Верно! Количество попыток - {i}")
+            break
+        attempt += 1
+        print(f"Осталось попыток {10-attempt}")
+    if attempt >= 10:
+        print(f"Попытки закончились! Было загадано {random_value}")
+
+
+random_value_game()
